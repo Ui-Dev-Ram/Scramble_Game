87cee3e4c58744b51b6fd273cf5556ec6035ddf1
Ui-Dev-Ram <145400237+Ui-Dev-Ram@users.noreply.github.com>
2023-10-13 15:26:44 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,58 @@
+let msg = document.querySelector('.msg');
+let guess = document.querySelector('input');
+let btn = document.querySelector('.btn')
+let randWords = "";
+let newWords = "";
+let play = false;
+
+let sWord = ["ramkumar", "ashokkumar", "dimplesingh", "lendysingh", "pabloescobar", "popla", "bhilu", "gungi", "mulla"]
+
+
+const createNewWord = () => {
+      let ranNum = Math.floor(Math.random() *sWord.length);
+      let newTempSword = sWord[ranNum];
+      return newTempSword;
+    }
+
+    
+
+const scrambleWords = (arr) => {
+     for (let i = arr.length-1; i >= 0; --i ){
+        let tempAre = arr[i];
+        let j = Math.floor(Math.random() * (i + 1))
+        
+        arr[i] = arr[j];
+        arr[j] = tempAre;
+     }  
+
+     return arr
+}    
+
+
+
+btn.addEventListener('click', function(){
+      if(!play){
+        play = true;
+        btn.innerText = "Guess";
+        guess.classList.toggle('hidden');
+        newWords = createNewWord(); 
+        randWords = scrambleWords(newWords.split("")).join('');
+        msg.innerHTML = `Guess the word  ' ${randWords} '`; 
+       }else{
+         let inputValue = guess.value;
+         if(inputValue === newWords) {
+            play = false;
+            msg.innerHTML = `Awesome It's Correct word. it is ${newWords}`;
+            btn.innerText = "Start again";
+            guess.classList.toggle('hidden');
+            guess.value = "";
+
+
+         }else{
+            msg.innerHTML = `Sorry It's Incorrect word. Plz try again ${randWords}`;
+            btn.innerText = "Try again";
+         }
+        
+       }
+
+})
